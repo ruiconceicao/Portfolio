ba86c009a11871143f923d6bee89fb35abf67df2
Rui Conceição <163507940+ruiconceicao@users.noreply.github.com>
2024-11-07 05:29:32 +0000

    Created _animations.css to set animations Updated all other css files Updated main page, index.html
    Done:
    - Top bar and menu feature finished
    
    To do:
    - Make font-size responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,24 @@ function toggleLightMode() {
       '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 25 25" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>';
   }
 }
+
 function toggleMenu() {
-  const buttonMenu = document.querySelector(".menuSmall .toggle");
-  const menuState = buttonMenu.value;
-  if (menuState === "inactive") {
+  const menuDrop = document.querySelector(".menuDrop");
+  const dataState = menuDrop.getAttribute("data-visible");
+  const buttonMenu = document.querySelector("button#toggleMenu");
+  const menuState = menuDrop.style.display;
+  if (dataState === "false") {
+    menuDrop.style.display = "block";
+    menuDrop.setAttribute("data-visible", "true");
     buttonMenu.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>';
-    buttonMenu.value = "active";
   } else {
+    /* 
+    this will also toggle the animation dropUp triggered by the data-visible 
+    and set display to none.
+    */
+    menuDrop.setAttribute("data-visible", "false");
     buttonMenu.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>';
-    buttonMenu.value = "inactive";
   }
 }
