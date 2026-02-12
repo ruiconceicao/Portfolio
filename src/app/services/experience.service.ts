dc25491a80bfc0bbf72d5201c47a64601f613f78
Rui Conceição <163507940+ruiconceicao@users.noreply.github.com>
2026-02-12 15:08:54 +0000

    Update experience.service.ts
    Experience update

--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -7,19 +7,26 @@ import { Experience } from '../models/experience.model';
 export class ExperienceService {
   experiences = signal<Experience[]>([
     {
+      position: 'IT Systems Engineer',
+      contractType: 'Full-time',
+      company: 'Banco Montepio',
+      workPlace: 'Hybrid',
+      timeSpan: 'Feb 2026 - Now',
+    },
+    {
+      position: 'IT Systems Engineer',
+      contractType: 'Trainee',
+      company: 'Banco Montepio',
+      workPlace: 'Hybrid',
+      timeSpan: 'May 2025 - Feb 2026',
+    },
+    {
       position: 'Full Stack Development and IT Support',
       contractType: 'Internship',
       company: 'Sindicato da Banca, Seguros e Tecnologias',
       workPlace: 'On-site',
       timeSpan: 'Apr 2024 - Jul 2024',
-    },
-    {
-      position: 'Junior IT Systems Engineer',
-      contractType: 'Trainee',
-      company: 'Banco Montepio',
-      workPlace: 'Hybrid',
-      timeSpan: 'May 2025 - Now',
-    },
+    }
   ]);
   constructor() {}
 }
